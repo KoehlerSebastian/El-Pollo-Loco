bac0b83adccc6f1b3555b3ff3fa3501fffd0639c
Sebastian Köhler <mailqkoehler-sebastian.de>
2022-11-28 12:33:31 +0100

    animate character

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -5,6 +5,7 @@ class MovableObject {
     height = 150;
     width = 100;
 
+    imageCache = [];
 
 
 
@@ -14,6 +15,16 @@ class MovableObject {
     loadImage(path){
         this.img = new Image();
         this.img.src = path;
+    }
+
+
+    loadImages(arr){
+        arr.forEach((path) => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+
     }
 
     moveRight() {
